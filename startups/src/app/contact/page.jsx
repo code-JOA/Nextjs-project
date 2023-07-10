752a59097b6432d3c5aa513307fe63955d5e4fb4
Joshua Owusu <joshbillion1@gmail.com>
2023-07-10 17:17:14 +0000

    Commit auto_update

--- startups/src/app/contact/page.jsx
+++ startups/src/app/contact/page.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const Contact = () => {
-  return (
-  <div className='contact'>contact</div>;
-  )
+  return <div className="contact">contact</div>;
 };
 
 export default Contact;
