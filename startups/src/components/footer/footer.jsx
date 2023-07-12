b76a15d4658f16d3a660a0a716fea21e3f522c5e
Joshua Owusu <joshbillion1@gmail.com>
2023-07-12 16:33:05 +0000

    Commit auto_update

--- startups/src/components/footer/footer.jsx
+++ startups/src/components/footer/footer.jsx
@@ -5,35 +5,39 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <div>©2023 josh.io All rights reserved.</div>
+      <div>josh.io All Rights Reserved</div>
       <div className={styles.social}>
         <Image
           src="/1.png"
           width={15}
           height={15}
           className={styles.icon}
-          alt="Lama Dev Facebook Account"
+          alt="josh.io"
+          Facebook
         />
         <Image
           src="/2.png"
           width={15}
           height={15}
           className={styles.icon}
-          alt="Lama Dev"
+          alt="josh.io"
+          Instagram
         />
         <Image
           src="/3.png"
           width={15}
           height={15}
           className={styles.icon}
-          alt="Lama Dev"
+          alt="josh.io"
+          LinkendIn
         />
         <Image
           src="/4.png"
           width={15}
           height={15}
           className={styles.icon}
-          alt="Lama Dev"
+          alt="josh.io"
+          youtube
         />
       </div>
     </div>
