f8015c2d571cf9c483bfc705316ae63979d9441f
Joshua Owusu <joshbillion1@gmail.com>
2023-07-12 15:53:08 +0000

    Commit auto_update

--- startups/src/app/page.jsx
+++ startups/src/app/page.jsx
@@ -4,14 +4,14 @@ import Hero from "public/hero.png";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <div className={styles.item}>
         <h1 className={styles.title}>Better Design For SAAS products</h1>
         <p className={styles.desc}>
           Turning your dreams into reality using NoCode
         </p>
       </div>
-      <button>Our Works</button>
+      <button className={styles.button}>Our Works</button>
       <div className={styles.item}>
         <Image src={Hero} alt="" className={styles.img} />
       </div>
