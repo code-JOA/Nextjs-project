be382f0597f4079f785599dc000a830b2d2a1907
Joshua Owusu <joshbillion1@gmail.com>
2023-07-12 13:34:40 +0000

    Commit auto_update

--- startups/src/components/footer/footer.jsx
+++ startups/src/components/footer/footer.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import styles from "/footer.module.css";
 
 const Footer = () => {
-  return;
-  <div className={styles.container}>josh.io All Rights Reserved</div>;
+  return (
+  <div className={styles.container}>josh.io All Rights Reserved</div>
   <div>
     <Image src="/1.png" width={15} height={15} alt="josh.io" />
-  </div>;
+  </div>
+  )
 };
 
 export default Footer;
