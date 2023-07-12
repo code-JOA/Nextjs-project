08776432a0e05eeb73eae40be38ed828f4c55a42
Joshua Owusu <joshbillion1@gmail.com>
2023-07-12 15:28:28 +0000

    Commit auto_update

--- startups/src/app/page.jsx
+++ startups/src/app/page.jsx
@@ -8,9 +8,10 @@ export default function Home() {
       <div className={styles.text}>
         <h1>Better Design For SAAS products</h1>
         <p>Turning your dreams into reality using NoCode</p>
-        <div className={styles.imgContainer}></div>
-        <Image src={Hero} alt="" className={styles.img} />
       </div>
+
+      <div className={styles.imgContainer}></div>
+      <Image src={Hero} alt="" className={styles.img} />
     </div>
   );
 }
