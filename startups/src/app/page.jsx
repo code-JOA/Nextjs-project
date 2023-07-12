75ec763680330a8fab02a23fa4445774bad0ff7d
Joshua Owusu <joshbillion1@gmail.com>
2023-07-12 14:04:51 +0000

    Commit auto_update

--- startups/src/app/page.jsx
+++ startups/src/app/page.jsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import styles from 'public/hero.png'
 
 export default function Home() {
-  return <div></div>;
+  return <>
+    <Image  src=''  alt='' />
+  </div>;
 }
