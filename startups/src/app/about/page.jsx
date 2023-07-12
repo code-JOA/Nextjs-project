2b3fd1033317bc32d0614fb679c0c69a4f77189f
Joshua Owusu <joshbillion1@gmail.com>
2023-07-12 16:49:43 +0000

    Commit auto_update

--- startups/src/app/about/page.jsx
+++ startups/src/app/about/page.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
 
 const About = () => {
   return;
   <div className={styles.container}>
-    <div className={styles.imgContainer}></div>
+    <div className={styles.imgContainer}>
+      <Image src="" fill={true} />
+    </div>
     <div className={styles.textContainer}></div>
   </div>;
 };
