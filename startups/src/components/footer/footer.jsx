6532d90f4e1652843bf62be21bb95f10dc0bc495
Joshua Owusu <joshbillion1@gmail.com>
2023-07-12 13:54:06 +0000

    Commit auto_update

--- startups/src/components/footer/footer.jsx
+++ startups/src/components/footer/footer.jsx
@@ -7,9 +7,24 @@ const Footer = () => {
     <div className={styles.container}>
       <div>josh.io All Rights Reserved</div>
       <div className={styles.imgContainer}>
-        <Image src="/1.png" fill={true} width={15} height={15} alt="josh.io" />
-        <Image src="/2.png" fill={true} width={15} height={15} alt="josh.io" />
+        <Image
+          src="/1.png"
+          fill={true}
+          width={15}
+          height={15}
+          alt="josh.io"
+          Facebook
+        />
+        <Image
+          src="/2.png"
+          fill={true}
+          width={15}
+          height={15}
+          alt="josh.io"
+          Instagram
+        />
         <Image src="/3.png" fill={true} width={15} height={15} alt="josh.io" />
+        <Image src="/1.png" fill={true} width={15} height={15} alt="josh.io" />
       </div>
     </div>
   );
