ceb0423b54e3c3b2de50d2d3e64da7de59515b06
Joshua Owusu <joshbillion1@gmail.com>
2023-07-14 14:11:34 +0000

    Commit auto_update

--- startups/src/app/portfolio/page.jsx
+++ startups/src/app/portfolio/page.jsx
@@ -5,10 +5,16 @@ import Link from "next/Link";
 const Portfolio = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.selectTitle}>Our Works</h1>;
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>;
       <div className={styles.items}>
-        <Link href="/portfolio/illustrations">
-          <span>illustrations</span>
+        <Link href="/portfolio/illustrations" className={styles.item}>
+          <span className={styles.title}>illustrations</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.item}>
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link href="/portfolio/application" className={styles.item}>
+          <span className={styles.title}>Application</span>
         </Link>
       </div>
     </div>
