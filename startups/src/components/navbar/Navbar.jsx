f972e65ffb96e584cc4f1e913ff335b49bf9d7cb
Joshua Owusu <joshbillion1@gmail.com>
2023-07-14 13:25:11 +0000

    Commit auto_update

--- startups/src/components/navbar/Navbar.jsx
+++ startups/src/components/navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 "use client";
 
-import React from "react";
 import Link from "next/link";
+import React from "react";
 import styles from "./navbar.module.css";
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
+import { signOut, useSession } from "next-auth/react";
 
 const links = [
   {
@@ -15,46 +17,48 @@ const links = [
     title: "Portfolio",
     url: "/portfolio",
   },
-
   {
     id: 3,
     title: "Blog",
     url: "/blog",
   },
-
   {
     id: 4,
     title: "About",
     url: "/about",
   },
-
   {
     id: 5,
     title: "Contact",
     url: "/contact",
   },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
 ];
 
 const Navbar = () => {
+  const session = useSession();
+
   return (
     <div className={styles.container}>
-      <Link className={styles.logo} href="/">
-        josh.io
+      <Link href="/" className={styles.logo}>
+        Josh.io
       </Link>
       <div className={styles.links}>
+        <DarkModeToggle />
         {links.map((link) => (
-          <Link key={link.id} href={link.url} className={styles.links}>
+          <Link key={link.id} href={link.url} className={styles.link}>
             {link.title}
           </Link>
         ))}
-        <button
-          className={styles.logout}
-          onClick={() => {
-            console.log("logged Out");
-          }}
-        >
-          Logout
-        </button>
+        {session.status === "authenticated" && (
+          <button className={styles.logout} onClick={signOut}>
+            Logout
+          </button>
+        )}
       </div>
     </div>
   );
