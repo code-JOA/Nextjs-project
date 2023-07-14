3854f87ba3ecb81e236a6e2ce4e65ccb2eb73a34
Joshua Owusu <joshbillion1@gmail.com>
2023-07-14 17:13:20 +0000

    Commit auto_update

--- startups/src/context/ThemeContext.js
+++ startups/src/context/ThemeContext.js
@@ -1,19 +1,1 @@
-"use client";
 
-import { createContext, useState } from "react";
-
-export const ThemeContext = createContext();
-
-export const ThemeProvider = ({ children }) => {
-  const [mode, setMode] = useState("dark");
-
-  const toggle = () => {
-    setMode((prev) => (prev === "dark" ? "light" : "dark"));
-  };
-
-  return (
-    <Theme.Provider value={{ toggle, mode }}>
-      <div className={`theme ${mode}`}>{children}</div>
-    </Theme.Provider>
-  );
-};
