37f3ea056cad8b920c3caeecb92e06674e7b7f66
Joshua Owusu <joshbillion1@gmail.com>
2023-07-14 16:59:35 +0000

    Commit auto_update

--- startups/src/app/layout.js
+++ startups/src/app/layout.js
@@ -15,9 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+                  <ThemeProvider>
+
         <div className="container">
           <Navbar />
-          <ThemeProvider>
             {children}
 
             <Footer />
