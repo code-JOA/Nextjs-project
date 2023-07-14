cb7f311e9bd24cd839b94c6b62848e7f28ae95cb
Joshua Owusu <joshbillion1@gmail.com>
2023-07-14 15:49:20 +0000

    Commit auto_update

--- startups/src/app/blog/page.jsx
+++ startups/src/app/blog/page.jsx
@@ -3,17 +3,17 @@ import styles from "./page.module.css";
 import Link from "next/link";
 import Image from "next/image";
 
-async function getData() {
-  const res = await fetch("http://localhost:3000/api/posts", {
-    cache: "no-store",
-  });
+// async function getData() {
+//   const res = await fetch("http://localhost:3000/api/posts", {
+//     cache: "no-store",
+//   });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
 
-  return res.json();
-}
+//   return res.json();
+// }
 
 const Blog = async () => {
   const data = await getData();
