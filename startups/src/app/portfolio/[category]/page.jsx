2c753890cce2e5aff7b4161ad2e80d05c645c6b3
Joshua Owusu <joshbillion1@gmail.com>
2023-07-14 14:56:47 +0000

    Commit auto_update

--- startups/src/app/portfolio/[category]/page.jsx
+++ startups/src/app/portfolio/[category]/page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Category = ({ params }) => {
   console.log(params);
   return (
-    <div>
+    <div className={styles.container}>
       <h1 className={styles.catTitle}>{params.Category}</h1>
     </div>
   );
