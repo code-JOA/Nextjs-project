7ed23784337d5c6d0cfb2c10f32456d649a35cdc
Joshua Owusu <joshbillion1@gmail.com>
2023-07-14 14:43:00 +0000

    Commit auto_update

--- startups/src/app/portfolio/layout.jsx
+++ startups/src/app/portfolio/layout.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
+import styles from './page.module.css'
 
-const>
- = () => {
+const layout = () => {
   return (
-    <div>
-        
+    <div className={}>
+        <h1 className={}>Our World</h1>
     </div>
   )
 }
 
-export default>
+export default layout
