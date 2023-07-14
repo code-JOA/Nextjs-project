95493927579d0604562f5e0c2c6fb3a48733b8d7
Joshua Owusu <joshbillion1@gmail.com>
2023-07-14 15:24:37 +0000

    Commit auto_update

--- startups/src/app/portfolio/[category]/page.jsx
+++ startups/src/app/portfolio/[category]/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./page.module.css";
 import Button from "@/components/Button/Button";
+import Image from "next/image";
 
 const Category = ({ params }) => {
   console.log(params);
