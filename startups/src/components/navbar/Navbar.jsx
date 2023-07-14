26b005098f7a2e7aa163ee826fe5a7b2235ae39d
Joshua Owusu <joshbillion1@gmail.com>
2023-07-14 18:11:47 +0000

    Commit auto_update

--- startups/src/components/navbar/Navbar.jsx
+++ startups/src/components/navbar/Navbar.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import Link from "next/link";
 import styles from "./navbar.module.css";
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
 
 const links = [
   {
@@ -42,6 +43,7 @@ const Navbar = () => {
         josh.io
       </Link>
       <div className={styles.links}>
+        <DarkModeToggle />
         {links.map((link) => (
           <Link key={link.id} href={link.url} className={styles.links}>
             {link.title}
