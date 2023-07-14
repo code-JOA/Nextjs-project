6b7b5aaf65ed2c29ed477138e7bf6e528ea29917
Joshua Owusu <joshbillion1@gmail.com>
2023-07-14 13:33:02 +0000

    Commit auto_update

--- startups/src/app/portfolio/page.jsx
+++ startups/src/app/portfolio/page.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./page.module.css";
+import Link from "next/Link";
 
 const Portfolio = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.selectTitle}>Our Works</h1>;
+      <div className={styles.items}></div>
     </div>
   );
 };
