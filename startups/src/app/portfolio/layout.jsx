64b885787498191d8f62ed2870b14ed9698e9d73
Joshua Owusu <joshbillion1@gmail.com>
2023-07-14 14:48:23 +0000

    Commit auto_update

--- startups/src/app/portfolio/layout.jsx
+++ startups/src/app/portfolio/layout.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styles from "./page.module.css";
 
-const layout = ({ children }) => {
+const Layout = ({ children }) => {
   return (
     <div>
-      <h1 className={styles.mainTitle}>Our World</h1>
+      <h1 className={styles.mainTitle}>Our Works</h1>
       {children}
     </div>
   );
 };
 
-export default layout;
+export default Layout;
